f95d1fee9b40e21c1c237417f4decaf7e5a9457a
kimchunyong <lovemy5854@naver.com>
2019-02-17 19:32:55 +0900

    filter

--- chapter.1/filter.js
+++ chapter.1/filter.js
@@ -47,3 +47,40 @@ console.log(users_under_30);
     name: "HI"
     }]
 */
+
+var ages = [];
+for (var i = 0, len = users_under_30.length; i < len; i++) {
+  ages.push(users_under_30[i].age);
+}
+
+console.log(ages);
+/* [25, 28, 27, 24] */
+
+var users_over_30 = filter(users, function (user) {
+  return user.age >= 30;
+});
+
+console.log(users_over_30);
+/*
+    [[object Object] {
+    age: 32,
+    id: 1,
+    name: "ID"
+    }, [object Object] {
+    age: 32,
+    id: 3,
+    name: "BJ"
+    }, [object Object] {
+    age: 32,
+    id: 6,
+    name: "JM"
+    }]
+*/
+
+var names = [];
+for (var i = 0, len = users_over_30.length; i < len; i++) {
+  names.push(users_over_30[i].name);
+}
+
+console.log(names);
+/* ["ID", "BJ", "JM"] */
