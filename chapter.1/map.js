1b4f4ae86d07d04d35add66b5768d2af5e6ad2e2
kimchunyong <lovemy5854@naver.com>
2019-02-21 00:06:11 +0900

    리팩토링

--- chapter.1/map.js
+++ chapter.1/map.js
@@ -56,3 +56,15 @@ var ages = map(users_under_30, function (user) {
 });
 
 console.log(ages); // [25, 28, 27, 24]
+
+var users_over_30 = filter(users, function (user) {
+  return user.age >= 30;
+});
+
+console.log(users_over_30);
+
+var names = map(users_over_30, function (user) {
+  return user.name;
+});
+
+console.log(names);
