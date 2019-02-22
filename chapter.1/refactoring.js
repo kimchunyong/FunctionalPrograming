87576742ae81a1cdf3c5960cd3b11c7732d90936
kimchunyong <lovemy5854@naver.com>
2019-02-22 23:44:36 +0900

    functional

--- chapter.1/refactoring.js
+++ chapter.1/refactoring.js
@@ -12,3 +12,19 @@ function bvalue (key) {
 }
 
 bvalue('a')({ a: 'hi', b: 'hello' });
+
+var users = [
+  { id: 1, name: 'ID', age: 32 },
+  { id: 2, name: 'HA', age: 25 },
+  { id: 3, name: 'BJ', age: 32 },
+  { id: 4, name: 'PJ', age: 28 },
+  { id: 5, name: 'JE', age: 27 },
+  { id: 6, name: 'JM', age: 32 },
+  { id: 7, name: 'HI', age: 24 }
+];
+
+console.log(
+  filter(users, function (user) {
+    return user.id === 3;
+  })[0]
+);
